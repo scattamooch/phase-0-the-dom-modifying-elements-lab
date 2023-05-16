b0407958b348e9d0dca204e6dc5e0097bb213ef4
scattamooch <71458730+scattamooch@users.noreply.github.com>
2023-05-16 09:08:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Remove the existing <main> element with ID "main"
+var main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element with ID "victory" and set its text content
+var newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Sean is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
